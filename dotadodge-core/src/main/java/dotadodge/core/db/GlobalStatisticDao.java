e4211a26fc71c6874e4df3a5a554a1350d8bd80c
armadco <armadco@gmail.com>
2015-12-16 01:14:54 +0300

    add smth

--- dotadodge-core/src/main/java/dotadodge/core/db/GlobalStatisticDao.java
+++ dotadodge-core/src/main/java/dotadodge/core/db/GlobalStatisticDao.java
@@ -5,22 +5,28 @@ import java.util.List;
 import dotadodge.core.model.Match;
 import dotadodge.core.model.external.PlayerGlobalDetails;
 import dotadodge.core.model.external.PlayersInPartyInfo;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class GlobalStatisticDao {
-    
+
     public List<Match> getMatchHistory(int id) {
-	// TODO: armad
-	return null;
+        // TODO: armad
+        return null;
     }
-    
+
     public List<PlayerGlobalDetails> getPlayers(int... id) {
-	//TODO: armad winrate, etc
-	return null;
+
+        Document doc;
+     //   doc = Jsoup.connect("http://google.com").get();
+        return null;
     }
-    
+
     public PlayersInPartyInfo getPlayersInPartyInfo(int... id) {
-	//TODO: armad
-	return null;
+        //TODO: armad
+        return null;
     }
 
 }
