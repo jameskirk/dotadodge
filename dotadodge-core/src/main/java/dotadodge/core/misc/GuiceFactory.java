72e24ee3b0c003bc89ee200e043787079e5ca1af
golovin <ilarion.golovin@t-systems.ru>
2015-12-17 18:36:58 +0300

    fixed JPA config

--- dotadodge-core/src/main/java/dotadodge/core/misc/GuiceFactory.java
+++ dotadodge-core/src/main/java/dotadodge/core/misc/GuiceFactory.java
@@ -6,7 +6,7 @@ import com.google.inject.persist.jpa.JpaPersistModule;
 
 public class GuiceFactory {
     
-    public static final String PU_NAME_TEST = "examplePU";
+    public static final String PU_NAME_TEST = "testPU";
     
     public static final String PU_NAME_PROD = "prodPU";
     
@@ -14,7 +14,7 @@ public class GuiceFactory {
     
     public static Injector getInjector() {
 	if (injector == null) {
-	    injector = Guice.createInjector(new JpaPersistModule(PU_NAME_TEST));
+	    injector = Guice.createInjector(new JpaPersistModule(PU_NAME_PROD));
 	}
         return injector;
     }
